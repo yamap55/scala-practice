72e0533d841bfe4f119fa0ff20cf078d769dd315
yamap55 <tetuya120@hotmail.com>
2017-06-28 23:12:34 +0900

    [add] range exercises for collection

--- src/com/yamap55/scala/CollectionSample.scala
+++ src/com/yamap55/scala/CollectionSample.scala
@@ -3,6 +3,25 @@ package com.yamap55.scala
 object CollectionSample {
   def main(args: Array[String]): Unit = {
     arrayMethod()
+    rangeMethod()
+  }
+
+  def rangeMethod(): Unit = {
+    println("--- Range Start ---")
+
+    val range1 = 1 to 5
+    println(range1)
+
+    val range2 = (1 to 5).toList
+    println(range2)
+
+    val range3 = 1 until 5
+    println(range3)
+
+    val range4 = (1 until 5).toList
+    println(range4)
+
+    println("--- Range End ---")
   }
 
   def arrayMethod(): Unit = {
