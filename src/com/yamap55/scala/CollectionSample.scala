2a867a079a677455050010acb9e1bf12b5bee489
yamap55 <tetuya120@hotmail.com>
2017-06-28 23:39:20 +0900

    [add] list exercises for collection

--- src/com/yamap55/scala/CollectionSample.scala
+++ src/com/yamap55/scala/CollectionSample.scala
@@ -4,6 +4,45 @@ object CollectionSample {
   def main(args: Array[String]): Unit = {
     arrayMethod()
     rangeMethod()
+    listMethod()
+  }
+
+  def listMethod(): Unit = {
+    println("--- List Start ---")
+    val list = List(1, 2, 3, 4, 5)
+    // list(0) = 99 // immutable
+
+    val a1 = 1 :: Nil
+    println(a1)
+
+    val a2 = 2 :: a1
+    println(a2)
+
+    val a3 = 3 :: a2
+    println(a3)
+
+    val a4 = 4 :: a3
+    println(a4)
+
+    val a5 = 5 :: a3
+    println(a5)
+
+    val a6 = 1 :: 2 :: 3 :: 4 :: Nil
+    println(a6)
+
+    val a7 = Nil.::(4).::(3).::(2).::(1)
+    println(a7)
+
+    val a8 = List(1, 2) ++ List(3, 4)
+    println(a8)
+
+    val a9 = List(1, 2).++(List(3, 4))
+    println(a9)
+
+
+
+
+    println("--- List End ---")
   }
 
   def rangeMethod(): Unit = {
